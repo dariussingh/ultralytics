3f247b8758b745f2c299be979a41b71e03bf0cb0
dariussingh <darius2121.ds@gmail.com>
2024-11-23 22:12:46 +0530

    update gitignore

--- main.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO(".\\ultralytics\\cfg\\models\\11\\yolo11n-mad.yaml")
-
-model.train(data=".\\ultralytics\\cfg\\datasets\\coco-mad.yaml", epochs=1)
-
-# model.val(data=".\\ultralytics\\cfg\\datasets\\coco-mad.yaml")
-
-# model.predict("./ultralytics/assets/bus.jpg")
